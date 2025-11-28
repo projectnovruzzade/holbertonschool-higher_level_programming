85c5eaa80ab8265d9ef4c519bc0d92ca504f530c
projectnovruzzade <tunarwork99@gmail.com>
2025-11-28 13:42:41 +0000

    task-9

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(letter):
+    lst = ""
+    for i in letter:
+        a = 0
+        if 97 <= ord(i) <= 122:
+            a = ord(i) - 32
+            lst += chr(a)
+        else:
+            lst += i
+    return lst
