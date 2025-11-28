262963e7304cfb91a1433df4d00677e8eeda11b6
projectnovruzzade <tunarwork99@gmail.com>
2025-11-28 13:17:54 +0000

    task-8

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -3,7 +3,7 @@ islower = __import__('7-islower').islower
 
 
 def islower(letter):
-    if 97 <= ord(letter) <= 122:
+    if 97 <= ord(letter) <= 122 or 0 <= ord(letter) < 10:
         return "lower"
     else:
         return "upper"
