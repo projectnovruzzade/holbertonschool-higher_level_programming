d6be14aee4c37cf1417aced1d7e724dc93657bd0
projectnovruzzade <tunarwork99@gmail.com>
2025-11-28 12:59:46 +0000

    task-7

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for k in range(i+1, 10):
         if i == 8 and k == 9:
-            print(f"{i}{k}")
+            print("{}{}".format(i, k))
         else:
-            print(f"{i}{k}", end=", ")
+            print("{}{}".format(i, k), end=", ")
