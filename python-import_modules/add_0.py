cb104ed596e0a52e84d0368ca7a9184e420cbc58
root <tunarwork99@gmail.com>
2025-12-01 11:19:19 +0000

    task-1

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add  # Import the function normally
-
-def main():
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+def add(a,b):
+    return a + b
 
 # This block runs only if the file is executed directly
 if __name__ == "__main__":
