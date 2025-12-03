c2b28a417ae769a3ef508fb70671fc566fc27be1
root <tunarwork99@gmail.com>
2025-12-03 22:20:59 +0000

    task-8

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    t = ()
-    c = 0
-    if len(tuple_b) == 0:
-        tuple_b = tuple_b + (0, 0, )
-    elif len(tuple_b) < 2:
-        tuple_b = tuple_b + (0, )
-    for a, b in zip(tuple_a, tuple_b):
-        t = t + (a + b,)
-        c += 1
-        if c == 2:
-            break
-    return t
+    # Ensure both tuples have at least 2 elements
+    a1 = tuple_a[0] if len(tuple_a) > 0 else 0
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+
+    b1 = tuple_b[0] if len(tuple_b) > 0 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    return (a1 + b1, a2 + b2)
