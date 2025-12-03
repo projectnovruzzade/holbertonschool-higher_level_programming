37711d0ba152b59a41c49ae4020f4be699e958b4
root <tunarwork99@gmail.com>
2025-12-03 20:46:24 +0000

    task-7

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [] or matrix == [[]]:
+        print("$")
+    else:
+        for i in range(len(matrix)):
+            a = ""
+            for k in range(len(matrix)):
+                a = a + " " + str(matrix[i][k])
+            print(a, end="$\n")
