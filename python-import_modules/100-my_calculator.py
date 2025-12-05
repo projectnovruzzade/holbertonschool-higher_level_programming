80e32497945597ebb5fd843e4411a530b0118e3d
root <tunarwork99@gmail.com>
2025-12-05 13:33:00 +0000

    task-advanced

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-import sys
+from sys import argv, exit
 from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    if len(sys.argv) > 4:
+    if len(argv) > 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    if sys.argv[2] == "+":
-        result = add(int(sys.argv[1]), int(sys.argv[3]))
-        print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {result}")
-    elif sys.argv[2] == "-":
-        result = sub(int(sys.argv[1]), int(sys.argv[3]))
-        print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {result}")
-    elif sys.argv[2] == "^":
-        result = mul(int(sys.argv[1]), int(sys.argv[3]))
-        print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {result}")
-    elif sys.argv[2] == "/":
-        result = div(int(sys.argv[1]), int(sys.argv[3]))
-        print(f"{sys.argv[1]} {sys.argv[2]} {sys.argv[3]} = {result}")
+    if argv[2] == "+":
+        result = add(int(argv[1]), int(argv[3]))
+        print(f"{argv[1]} {argv[2]} {argv[3]} = {result}")
+    elif argv[2] == "-":
+        result = sub(int(argv[1]), int(argv[3]))
+        print(f"{argv[1]} {argv[2]} {argv[3]} = {result}")
+    elif argv[2] == "^":
+        result = mul(int(argv[1]), int(argv[3]))
+        print(f"{argv[1]} {argv[2]} {argv[3]} = {result}")
+    elif argv[2] == "/":
+        result = div(int(argv[1]), int(argv[3]))
+        print(f"{argv[1]} {argv[2]} {argv[3]} = {result}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
