356aa45fe7686f7972857de28d9afbc063d46ce4
root <tunarwork99@gmail.com>
2025-12-11 11:11:21 +0000

    task-13

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     example = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    if len(roman_string) == 1:
-        return example[roman_string]
-    total = example[roman_string[0]]
-    temp = example[roman_string[0]]
-    for i in range(1, len(roman_string)):
-        if temp - example[roman_string[i]] >= 0:
-            total = total + example[roman_string[i]]
-            temp = example[roman_string[i]]
+    total = 0
+    for i in range(len(roman_string)):
+        element = roman_string[i]
+        if example[element] < example[roman_string[i + 1]]:
+            if i + 1 < len(roman_string):
+                total -= example[roman_string[i]]
         else:
-            total = example[roman_string[i]] - total
+            total += example[roman_string[i]]
     return total
