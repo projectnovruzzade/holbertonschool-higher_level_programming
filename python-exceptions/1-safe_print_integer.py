b18a633a78e43273ce504baacef42fc65f3b047f
root <tunarwork99@gmail.com>
2025-12-12 13:53:52 +0000

    task-2

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if type(value) is int:
+        if value * value:
             print("{:d}".format(value))
             return True
         else:
             return False
     except Exception:
-            print()
+        return False
