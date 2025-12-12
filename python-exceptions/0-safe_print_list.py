6f60d35e7f330604ec572d528c72f33db4294732
root <tunarwork99@gmail.com>
2025-12-12 13:31:22 +0000

    task-1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x == 0:
-        return 0
-    c = 0
+    count = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
-            c += 1
-            if c == x:
-                print("", end="\n")
-    except Exception:
-        print("", end="\n")
-    return c
+            try:
+                print(my_list[i], end="")
+                count += 1
+            except Exception:
+                break
+    finally:
+        print()
+    return count
