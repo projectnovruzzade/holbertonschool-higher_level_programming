1410ff0a66c3d971e61cfe06c27b2cc23992aeca
projectnovruzzade <tunarowork99@gmail.com>
2025-12-21 19:21:24 +0000

    task-10

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -80,7 +80,7 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-        
+
     @classmethod
     def square(cls, size=0):
         """Return a new Rectangle instance where width == height == size"""
