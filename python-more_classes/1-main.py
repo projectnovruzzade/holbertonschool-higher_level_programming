4d5547a72828933fd50dc7e83e7ae1e603522188
projectnovruzzade <tunarwork99@gmail.com>
2025-12-21 17:26:08 +0000

    task-2

--- python-more_classes/1-main.py
+++ python-more_classes/1-main.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 Rectangle = __import__('1-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
-print(my_rectangle.__dict__)
+# my_rectangle = Rectangle(2, 4)
+# print(my_rectangle.__dict__)
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle.__dict__)
+# my_rectangle.width = 10
+# my_rectangle.height = 3
+# print(my_rectangle.__dict__)
 
+# try:
+#     my_rectangle = Rectangle(2, 3)
+#     my_rectangle.width = -5
+#     print(my_rectangle.__dict__)
+# except Exception as e:
+#     print("[{}] {}".format(e.__class__.__name__, e))
 try:
-    my_rectangle = Rectangle(2, 3)
-    my_rectangle.width = -4
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-try:
-    my_rectangle = Rectangle(2, 3)
-    my_rectangle.width = "4"
+    my_rectangle = Rectangle(-2, 3)
+    print(my_rectangle.__dict__)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-is it work modular
+Defines a Rectangle class
 """
 
 
-class Rectangle():
-    """
-    is it work class
-    """
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
+class Rectangle:
+    """Represents a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -18,9 +17,9 @@ class Rectangle():
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -30,7 +29,7 @@ class Rectangle():
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
