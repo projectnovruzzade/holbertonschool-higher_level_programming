037258d75df01d550523a7a573a88543e467c904
root <tunarwork99@gmail.com>
2025-12-24 10:43:02 +0000

    task-4

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+this is global enviroment
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    this is function enviroment
+    """
+    return isinstance(obj, a_class)
