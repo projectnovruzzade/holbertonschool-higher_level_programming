15f4d30a4dd403e3e28f15308f969323768d9593
root <tunarwork99@gmail.com>
2025-12-24 10:11:26 +0000

    task-1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -9,7 +9,8 @@ class MyList(list):
         this is local enviroment
     """
     def __init__(self, *args):
-        super().__init__(self, *args)
+        """Prints the list in ascending order"""
+        super().__init__(*args)
 
     def append(self, value):
         super().append(value)
