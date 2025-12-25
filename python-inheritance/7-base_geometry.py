d491c54c2c199fa0d87f9bcca86af9f99fabdbfa
root <tunarwork99@gmail.com>
2025-12-25 12:58:24 +0000

    task-8

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+this is global enviroment
+"""
+
+
+class BaseGeometry:
+    """
+        this is local enviroment
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
