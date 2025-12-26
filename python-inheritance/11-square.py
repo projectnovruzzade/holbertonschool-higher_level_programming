4859f8802aa234ff4d05096757813ecec5d58eec
root <tunarowork99@gmail.com>
2025-12-26 16:34:19 +0000

    task-12

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+this is global enviroment
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+        this is global enviroment
+    """
+    def __init__(self, size):
+        self.__size = size
+
+        self.integer_validator("size", self.__size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
