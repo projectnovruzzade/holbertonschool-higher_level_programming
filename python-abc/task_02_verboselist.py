5eddbda8da88764c8a1a6d8c0d2018a9fd511e91
root <tunarowork99@gmail.com>
2025-12-28 18:26:07 +0000

    task-3

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+this is global enviroment
+"""
+
+
+class VerboseList(list):
+    """
+    this is global enviroment
+    """
+
+    def append(self, element):
+        super().append(element)
+        print(f"Added [{element}] to the list.")
+
+    def extend(self, elements):
+        for element in elements:
+            super().append(element)
+        print(f"Extended the list with [{len(elements)}] items.")
+
+    def remove(self, element):
+        super().remove(element)
+        print(f"Removed [{element}] from the list.")
+
+    def pop(self, index=-1):
+        temp = super().pop(index)
+        print(f"Popped [{temp}] from the list.")
+
+    def __str__(self):
+        return f"array {super().__str__()}"
