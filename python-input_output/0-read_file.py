382dae69619eaf8f3f94549586d21c06c5d35d44
root <tunarwork99@gmail.com>
2025-12-29 15:23:20 +0000

    task-1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     """
     this is internal enviroment
     """
-    with open("my_file_0.txt", "r", encoding="utf-8") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         data = file.read()
         print(data)
