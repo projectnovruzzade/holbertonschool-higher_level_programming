cea1dd854964e4cd29eaea5eb143e20c69bf0921
root <tunarwork99@gmail.com>
2025-12-30 13:27:37 +0000

    task-6

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Returns the JSON representation of an object."""
     with open(filename, "w") as file:
         return json.dump(my_obj, file)
