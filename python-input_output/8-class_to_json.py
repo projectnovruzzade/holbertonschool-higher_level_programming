07e7907e476e70cd259188ce47b3345c93a19fcc
root <tunarwork99@gmail.com>
2026-01-03 11:56:53 +0000

    task-9

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Adds all command line arguments to a Pyfile."""
 
-import json
 
 def class_to_json(obj):
     """Adds all command line arguments to a Pyfile."""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
