083475e314b7c950dd79f6206858ff3f0719d687
root <tunarwork99@gmail.com>
2026-01-03 11:27:26 +0000

    task-8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,9 +6,7 @@ import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-items = load_from_json_file("add_item.json")
+
 load_from_json_file("add_item.json")
 
-items.extend(sys.argv[1:])
-
-save_to_json_file(items)
+save_to_json_file(sys.argv[1:])
