7483fe925c102c5ee2a00e1b7a9dc99dfc372874
root <tunarwork99@gmail.com>
2026-01-03 14:16:21 +0000

    task-11

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Adds all command line arguments to a Pyfile."""
+
+
+class Student():
+    """Adds all command line arguments to a Pyfile."""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        temp = {}
+        data = self.__dict__
+        if not attrs:
+            return self.__dict__
+        for element in self.__dict__:
+            if element in attrs:
+                temp[element] = data[element]
+        return temp
