513cc058d070db367e78075188dfbf8bcc304550
root <tunarwork99@gmail.com>
2026-01-03 11:25:18 +0000

    task-8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Returns the JSON representation of an object."""
+"""Adds arguments to a list and saves them to a JSON file."""
 
 import sys
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-
 
 items = load_from_json_file("add_item.json")
 load_from_json_file("add_item.json")
