1870d0f2da5fb43fb881269c0d3d4caa6310189d
root <tunarwork99@gmail.com>
2026-01-04 14:55:38 +0000

    task-2

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Load and return an instance of CustomObject from a file."""
+
+import pickle
+
+
+class CustomObject():
+    """Load and return an instance of CustomObject from a file."""
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        with open(filename, "wb") as file:
+            pickle.dump(self, file)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """Load and return an instance of CustomObject from a file."""
+        with open(filename, "rb") as file:
+            return pickle.load(file)
