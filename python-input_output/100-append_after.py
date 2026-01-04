1bc24dd1d16852acf657cdea38e920b38fdb89c8
root <tunarwork99@gmail.com>
2026-01-04 15:46:58 +0000

    task-advanced

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+def append_after(filename="", search_string="", new_string=""):
+    """
+    this is internal
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+
+    with open(filename, "w", encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string + "\n")
