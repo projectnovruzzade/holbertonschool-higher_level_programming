70b7d43c2297ac93bccf1d0597d52a25d081e7d6
root <tunarwork99@gmail.com>
2026-01-22 21:47:07 +0000

    task_3

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- example
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT SELECT ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- example
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2 "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
+CREATE USER IF NOT EXISTS hbtn_0d_2 "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
 GRANT SELECT PRIVILEGES ON hbtn_0d_2.* TO "user_0d_2"@"localhost";
 FLUSH PRIVILEGES;
