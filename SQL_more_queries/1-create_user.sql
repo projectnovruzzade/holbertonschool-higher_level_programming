e6d9ef4b51ecb8ca66a9c653bd0eef868c813f2a
root <tunarwork99@gmail.com>
2026-01-22 21:48:30 +0000

    task_3

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- example
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT SELECT ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
