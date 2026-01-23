78261abd5e3fdf8146e85c3bb1e81b0645d368f3
root <tunarwork99@gmail.com>
2026-01-23 21:14:36 +0000

    task_16

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- example
+SELECT s.title
+FROM tv_shows AS s
+INNER JOIN tv_show_genres AS sg ON s.id = sg.show_id
+INNER JOIN tv_genres AS g ON g.id = sg.genre_id
+WHERE g.name = 'Comedy'
+ORDER BY s.title ASC;
