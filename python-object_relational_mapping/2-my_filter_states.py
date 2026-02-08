d491858c676422b3b5931809e67eb66af1ac14d0
root <tunarwork99@gmail.com>
2026-02-08 19:27:11 +0000

    task_3

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+this is global enviroment
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+        this is local enviroment
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    query = f'SELECT * FROM states WHERE name = "{name}"'
+    cursor.execute(query)
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
