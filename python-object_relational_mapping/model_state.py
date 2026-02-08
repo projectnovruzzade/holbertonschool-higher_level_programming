eb6382d41b5fced07b292acdd6bd66c157d39bfb
root <tunarwork99@gmail.com>
2026-02-08 22:14:08 +0000

    task_7

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
-this is external  
+this is external
 """
 
-from sqlalchemy  import Column, Integer, String
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """
-     this is local
+    this is local
     """
+
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
-    name = Column(String(128),nullable=False)
-
+    name = Column(String(128), nullable=False)
