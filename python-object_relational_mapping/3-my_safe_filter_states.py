3db2ce680bb50fb833e0ced8a44faedbf336fea0
root <tunarwork99@gmail.com>
2026-02-08 19:50:25 +0000

    task_4

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+this is global enviroment
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+        this is local enviroment
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    query = 'SELECT * FROM states WHERE BINARY name = %s'
+    cursor.execute(query, (name, ))
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
