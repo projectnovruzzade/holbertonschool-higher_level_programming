f1de65585f3233e120a73273f1a078f990cfc7c8
root <tunarwork99@gmail.com>
2026-02-08 19:31:42 +0000

    task_3

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    query = 'SELECT * FROM states WHERE name = "{name}"'.format(name)
+    query = 'SELECT * FROM states WHERE name = "{}"'.format(name)
     cursor.execute(query)
 
     states = cursor.fetchall()
