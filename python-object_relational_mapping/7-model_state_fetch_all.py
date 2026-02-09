f4b9a145fe338f5523d13932258dd8320b582707
root <tunarwork99@gmail.com>
2026-02-09 17:03:50 +0000

    task_8

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+this is external
+"""
+
+
+import sys
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+usm = sys.argv[1]
+pwd = sys.argv[2]
+dbnm = sys.argv[3]
+
+
+engine = create_engine(
+    f"mysql+pymysql://{usm}:{pwd}@localhost:3306/{dbnm}?charset=utf8mb4",
+    echo=True,
+    pool_pre_ping=True
+)
+
+Base.metadata.create_all(engine)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+stmt = select(State).group_by(State.id)
+result = session.execute(stmt)
+datas = result.scalars().all()
+for data in datas:
+    print("{}: {}".format(data.id, data.name))
--- python-object_relational_mapping/app.sql
+++ python-object_relational_mapping/app.sql
@@ -6,6 +6,4 @@
 --     name VARCHAR(256)
 -- );
 
-CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
-USE hbtn_0e_6_usa;
-SHOW CREATE TABLE states;
+-- CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
