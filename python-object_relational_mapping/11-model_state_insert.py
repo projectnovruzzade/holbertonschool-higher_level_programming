5cfe414ba696f27488b65861c6ef9da657e94fcb
root <tunarowork99@gmail.com>
2026-02-09 20:41:11 +0000

    task_12

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -9,11 +9,15 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+
     usnm = argv[1]
     pwd = argv[2]
     db = argv[3]
 
-    engine = create_engine(f"mysql+mysqldb://{usnm}:{pwd}@localhost:3306/{db}")
+    engine = create_engine(
+        f"mysql+mysqldb://{usnm}:{pwd}@localhost:3306/{db}",
+        future=True
+    )
 
     Session = sessionmaker(bind=engine, future=True)
     session = Session()
