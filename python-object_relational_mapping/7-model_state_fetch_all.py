bd421929f3abff7ae843a9bf82253a7e0029b33f
root <tunarwork99@gmail.com>
2026-02-09 17:07:03 +0000

    task_7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,27 +9,28 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+if __name__ == "__main__":
+    """
+    this is external
+    """
+    usm = sys.argv[1]
+    pwd = sys.argv[2]
+    dbnm = sys.argv[3]
 
-usm = sys.argv[1]
-pwd = sys.argv[2]
-dbnm = sys.argv[3]
+    engine = create_engine(
+        f"mysql+pymysql://{usm}:{pwd}@localhost:3306/{dbnm}?charset=utf8mb4",
+        echo=True,
+        pool_pre_ping=True
+    )
 
+    Base.metadata.create_all(engine)
 
-engine = create_engine(
-    f"mysql+pymysql://{usm}:{pwd}@localhost:3306/{dbnm}?charset=utf8mb4",
-    echo=True,
-    pool_pre_ping=True
-)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-Base.metadata.create_all(engine)
+    stmt = select(State).group_by(State.id)
+    result = session.execute(stmt)
+    datas = result.scalars().all()
 
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-
-stmt = select(State).group_by(State.id)
-result = session.execute(stmt)
-datas = result.scalars().all()
-for data in datas:
-    print("{}: {}".format(data.id, data.name))
+    for data in datas:
+        print("{}: {}".format(data.id, data.name))
