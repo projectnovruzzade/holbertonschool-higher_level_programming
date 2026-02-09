7b49d84da035714fdc988d56b672baaf4ff1d2b2
root <tunarowork99@gmail.com>
2026-02-09 20:52:37 +0000

    task_13

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+List all State objects from the database
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    usnm = argv[1]
+    pwd = argv[2]
+    db = argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{usnm}:{pwd}@localhost:3306/{db}",
+        future=True
+    )
+
+    Session = sessionmaker(bind=engine, future=True)
+    session = Session()
+
+    state = session.query(State).filter(State.id == 2).first()
+
+    if state:
+        state.name = "New Mexico"
+        session.commit()
+
+    session.close()
