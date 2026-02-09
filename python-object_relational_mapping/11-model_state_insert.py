86fdf52ab413b733f0bd29033f97571be1987c95
root <tunarowork99@gmail.com>
2026-02-09 20:44:15 +0000

    task_12

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -22,10 +22,10 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine, future=True)
     session = Session()
 
-    new_state = State("Louisiana")
-
+    new_state = State(name="Louisiana")
     session.add(new_state)
-
     session.commit()
 
-    print(new_state.id)
+    print("{}".format(new_state.id))
+
+    session.close()
